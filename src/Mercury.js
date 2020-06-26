5bfc0a267e08e3a3d6dfe060b058b95255bb0870
Kate Skorija <kateskorija@gmail.com>
2020-06-26 17:41:49 -0500

    remove deleted test and js files

--- src/Mercury.js
+++
@@ -1,8 +0,0 @@
-import { Earth } from './../src/Earth.js';
-
-export class Mercury {
-  constructor(earthAge, yearsLeftOnEarth) {
-    this.age = earthAge * .24
-    this.yearsLeft = yearsLeft
-  }
-}
